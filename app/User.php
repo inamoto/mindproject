f2f432c5f2083fa833bf374ddfb0079d8a8d5ef8
Hirohisa Inamoto <hirohisa.inamoto@gmail.com>
2017-08-06 18:02:26 +0900

    remove passport

--- app/User.php
+++ app/User.php
@@ -2,15 +2,14 @@
 
 namespace App;
 
-use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 //use Carbon\Carbon;
 
 class User extends Authenticatable
 {
-    //use Notifiable;
-     use HasApiTokens, Notifiable;
+    use Notifiable;
+    //use HasApiTokens, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
