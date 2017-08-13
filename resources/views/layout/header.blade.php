d652caa3d5e68b844a734d0d2d802ec08c9cb32d
Hirohisa Inamoto <hirohisa.inamoto@gmail.com>
2017-08-14 01:44:23 +0900

    modify link of Dashboard

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -9,7 +9,7 @@
 				            <span class="icon-bar"></span>
 				            <span class="icon-bar"></span>
 			            </button>
-			            <a class="navbar-brand" href="">MindProject</a>
+			            <a class="navbar-brand" href="/mindproject/public/home">MindProject</a>
 		            </div>
                     <div class="collapse navbar-collapse" id="navbarEexample">
                         <ul class="nav navbar-nav">
