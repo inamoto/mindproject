7ef1fdd61ab0ee4b4aca23079158fc6c5a18e64e
Hirohisa Inamoto <hirohisa.inamoto@gmail.com>
2017-08-14 01:08:58 +0900

    change favicon

--- resources/views/layout/body.blade.php
+++ resources/views/layout/body.blade.php
@@ -5,6 +5,7 @@
     <title>MindProject</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
     <!--[if lt IE 9]>
         <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
